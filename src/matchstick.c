99f92a2cfeed1b3d23c3f0061e594e8983d9372a
Vincent Attali <vincent.attali@epitech.eu>
2018-01-30 15:24:23 +0100

    "partial display"

--- src/matchstick.c
+++ src/matchstick.c
@@ -7,21 +7,33 @@
 
 #include "matchstick.h"
 
-void free_map(char **map, int lines)
+void display_map(int lines, int chars)
 {
-	for (int i = 0; i < lines; i++)
-		free(map[i]);
-	free(map);
-}
+	int map[lines];
+	int j = 0;
 
-void create_map(int lines, int chars)
-{
-	int center = chars / 2;
-	char **map = malloc(sizeof(char*) * lines);
-
-	for (int i = 0; i < lines; i++)
-		map[i] = malloc(sizeof(char) * chars);
-	free_map(map, lines);
+	lines += 2;
+	for (int i = 0; i < lines; i++) {
+		if (i == 0 || i == lines - 1)
+			map[i] = 0;
+		else
+			map[i] = 2 * i - 1;
+	}
+	for (int i = 0; i < lines; i++) {
+		if (map[i] == 0) {
+			for (int i = 0; i < chars; i++)
+				my_putchar('*');
+			my_putchar('\n');
+		} else {
+			my_putchar('*');
+			for (int i = 0; i < (chars - 2) / 2 - j; i++)
+				my_putchar(' ');
+			for (int k = 0; k < map[i]; k++)
+				my_putchar('|');
+			my_putchar('\n');
+			j++;
+		}
+	}
 }
 
 int main(int ac, char **av)
@@ -34,13 +46,13 @@ int main(int ac, char **av)
 		write(2, "Usage: ./matchstick <lines> <matches_per_turn>\n", 47);
 		return (84);
 	}
-	lines = my_getnbr(av[1]) + 2;
+	lines = my_getnbr(av[1]);
 	if (lines < 1 || lines > 99)
 		return (84);
 	matches = my_getnbr(av[2]);
-	chars = lines * 2 + 2;
+	chars = lines * 2 + 1;
 	if (matches < 1 || matches > chars - 2)
 		return (84);
-	create_map(lines, chars);
+	display_map(lines, chars);
 	return (0);
 }
