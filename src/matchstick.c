850064f99f99da2107b449543476750cbdddc102
Vincent Attali <vincent.attali@epitech.eu>
2018-01-31 10:35:13 +0100

    "fixed norm errors"

--- src/matchstick.c
+++ src/matchstick.c
@@ -7,37 +7,47 @@
 
 #include "matchstick.h"
 
+void stars_line(int chars)
+{
+	for (int i = 0; i < chars; i++)
+		my_putchar('*');
+	my_putchar('\n');
+}
+
+void matches_line(int *map, int chars, int i, int j)
+{
+	int k = 0;
+
+	my_putchar('*');
+	for (int i = 0; i < (chars - 2) / 2 - j; i++) {
+		my_putchar(' ');
+		k++;
+	}
+	for (int j = 0; j < map[i]; j++)
+		my_putchar('|');
+	for (int j = 0; j < chars - k - map[i] - 2; j++)
+		my_putchar(' ');
+	my_putchar('*');
+	my_putchar('\n');
+}
+
 void display_map(int lines, int chars)
 {
 	int map[lines];
 	int j = 0;
-	int k = 0;
 
 	lines += 2;
 	for (int i = 0; i < lines; i++) {
 		if (i == 0 || i == lines - 1)
-			map[i] = 0;
+			map[i] = -1;
 		else
 			map[i] = 2 * i - 1;
 	}
 	for (int i = 0; i < lines; i++) {
-		if (map[i] == 0) {
-			for (int i = 0; i < chars; i++)
-				my_putchar('*');
-			my_putchar('\n');
-		} else {
-			my_putchar('*');
-			for (int i = 0; i < (chars - 2) / 2 - j; i++) {
-				my_putchar(' ');
-				k++;
-			}
-			for (int j = 0; j < map[i]; j++)
-				my_putchar('|');
-			for (int j = 0; j < chars - k - map[i] - 2; j++)
-				my_putchar(' ');
-			my_putchar('*');
-			my_putchar('\n');
-			k = 0;
+		if (map[i] == -1)
+			stars_line(chars);
+		else {
+			matches_line(map, chars, i, j);
 			j++;
 		}
 	}
